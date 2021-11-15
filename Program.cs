98074c51a4ce91b44fae0767de872eeea6bd39a5
lostedzahfia <oyekunlefarouk2001@gmail.com>
2021-11-15 11:56:49 +0100

    created a new csharp program, and add a new line of code to it

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GITCLASS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("arnold trent");
+        }
+    }
+}
